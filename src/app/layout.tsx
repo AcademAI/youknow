efe8abf1c3290ce88c348098f038d2eaa8cfdb6e
ArsenKakasyan <archee.busy@gmail.com>
2023-11-18 20:18:11 +0300

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Lexend } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import { Provider } from "@/components/Providers";
 import { Toaster } from "@/components/ui/toaster";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 const lexend = Lexend({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={cn(lexend.className, "antialiased min-h-screen pt-16")} suppressHydrationWarning={true}>
         <Provider>
           <Navbar />
+          <GoogleAnalytics GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}/>
           {children}
           <Toaster />
         </Provider>
